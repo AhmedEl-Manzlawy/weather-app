6f69bdbc3e1e9134c1e0c7c202652f9db690e88a
Ahmed Elmanzlawy <ahmed.abdelnaser.elmanzlawy@gmail.com>
2024-02-11 19:52:11 +0200

    new

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -14,8 +14,8 @@ const Weather = ({data}) => {
         <img alt="icon" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
       </div>
       <div className="bottom">
-        <p className="des">{data.weather[0].description}</p>
-
+        <p className="des">{<data className="weather descripti" ></data>}</p>
+      
         <p className="min-max">H:{Math.round(data.main.temp_max)}°C / L:{Math.round(data.main.temp_min)}°C</p>
       </div>
     </div>
